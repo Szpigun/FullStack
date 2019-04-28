29b72ee378a37db1011193f07b1996fb6ef53061
student5 <student5@pcoip.ki.agh.edu.pl>
2019-04-28 11:31:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="app">
+    <h1>
+      <img src="./assets/logo.svg" alt="Enroller" class="logo">
+      System do zapisów na zajęcia
+    </h1>
+    <div v-if="authenticatedUsername">
+      <h2>Witaj {{ authenticatedUsername }}!
+        <a @click="logout()" class="float-right  button-outline button">Wyloguj</a>
+      </h2>
+      <meetings-page :username="authenticatedUsername"></meetings-page>
+    </div>
+    <div v-else>
+      <button @click="registering = false" :class="registering ? 'button-outline' : ''">Loguję się</button>
+      <button @click="registering = true" :class="!registering ? 'button-outline' : ''">Rejestruję się</button>
+      <div :class="'alert alert-' + (this.isError ? 'error' : 'success')" v-if="message">{{ message }}</div>
+      <login-form @submit="registering ? register($event) : login($event)" :button-label="loginButtonLabel"></login-form>
+    </div>
+  </div>
+</template>
+
+<script>
+    import "milligram";
+    import LoginForm from "./LoginForm";
+    import MeetingsPage from "./meetings/MeetingsPage";
+
+    export default {
+        components: {LoginForm, MeetingsPage},
+        data() {
+            return {
+                authenticatedUsername: "",
+                registering: false,
+                message: '',
+                isError: false
+            };
+        },
+        methods: {
+            register(user) {
+                this.clearMessage();
+                this.$http.post('participants', user)
+                    .then(() => {
+                        this.success('Konto zostało założone. Możesz się zalogować.');
+                        this.registering = false;
+                    })
+                    .catch(response => this.failure('Błąd przy zakładaniu konta. Kod odpowiedzi: ' + response.status));
+            },
+            login(user) {
+                this.clearMessage();
+                this.authenticatedUsername = user.login;
+            },
+            logout() {
+                this.authenticatedUsername = '';
+            },
+            success(message) {
+                this.message = message;
+                this.isError = false;
+            },
+            failure(message) {
+                this.message = message;
+                this.isError = true;
+            },
+            clearMessage() {
+                this.message = undefined;
+            }
+        },
+        computed: {
+            loginButtonLabel() {
+                return this.registering ? 'Zarejestruj się' : 'Zaloguj się';
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+  #app {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+
+  .logo {
+    vertical-align: middle;
+  }
+
+  .alert {
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 2px solid black;
+    &-success {
+      background: lightgreen;
+      border-color: darken(lightgreen, 10%);
+    }
+    &-error {
+      background: indianred;
+      border-color: darken(indianred, 10%);
+      color: white;
+    }
+  }
+
+</style>
+
